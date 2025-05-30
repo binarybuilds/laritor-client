1a5ab2700e04540fcb04099c03db5e94ed096d2f
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2025-05-29 23:02:51 -0400

    code cleanup

--- testbench_path.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-
-// This will give you the real Application instance Testbench uses
-$app = require __DIR__.'/vendor/orchestra/testbench-core/laravel/bootstrap/app.php';
-
-// Now resolve the @testbench base path
-echo $app->basePath('@testbench'), PHP_EOL;
