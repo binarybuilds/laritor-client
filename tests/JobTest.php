0196b4331524aaaed12a834b236e812d255a968b
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2025-08-02 00:03:34 -0400

    updated failed job assertion

--- tests/JobTest.php
+++ tests/JobTest.php
@@ -23,7 +23,7 @@ class JobTest extends TestCase
 
     public function test_it_records_failed_jobs(): void
     {
-        $this->get('/laritor-failed-job')->assertStatus(200);
+        $this->get('/laritor-failed-job')->assertStatus(500);
 
         $file = __DIR__.'/payloads/events.json';
         $this->assertFileExists($file);
