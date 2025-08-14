66f8af1a2eecb6b570d3f142e034c3e24919d9c5
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2025-08-14 00:24:17 -0400

    fix static analysis

--- tests/SchemaTest.php
+++ tests/SchemaTest.php
@@ -19,7 +19,7 @@ class SchemaTest extends TestCase
         $this->assertArrayHasKey('db_schema', $data['data']);
         $this->assertNotEmpty( $data['data']['db_schema']);
         $this->assertArrayHasKey('database', $data['data']['db_schema']);
-        $driver =  (int)app()->version() >= 9 ? DB::getDriverName() : config('database.connections.'.config('database.default').'driver');
+        $driver =  (int)app()->version() >= 9 ? DB::getDriverName() : config('database.connections.'.config('database.default').'.driver');
         $this->assertSame($driver, $data['data']['db_schema']['database']);
         $this->assertArrayHasKey('version', $data['data']['db_schema']);
         $this->assertArrayHasKey('tables', $data['data']['db_schema']);
