d7887806bd221c0fc085c336a145666b02a80f3d
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2026-04-12 00:43:19 -0400

    bump version

--- src/Laritor.php
+++ src/Laritor.php
@@ -13,7 +13,7 @@ use BinaryBuilds\LaritorClient\Recorders\SchedulerRecorder;
 
 class Laritor
 {
-    public const VERSION = '3.0.0';
+    public const VERSION = '3.0.1';
 
     /**
      * @var array
