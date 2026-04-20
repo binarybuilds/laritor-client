07f1f6777ede48d473705c10aa1db2582305c15c
Srinath Reddy Dudi <srinathreddydudi@gmail.com>
2026-04-19 23:12:05 -0400

    fix tests

--- src/Recorders/OutboundRequestRecorder.php
+++ src/Recorders/OutboundRequestRecorder.php
@@ -88,8 +88,9 @@ class OutboundRequestRecorder extends Recorder
             ->map(function ($request) use ($outboundRequestEvent){
 
             if ( $request['status'] === 'sent' && $request['url'] === $outboundRequestEvent->request->url() ) {
-                $request['started_at'] = $request['started_at']->format('Y-m-d H:i:s');
-                $request['duration'] = $request['started_at']->diffInMilliseconds();
+                $started = $request['started_at'];
+                $request['started_at'] = $started->format('Y-m-d H:i:s');
+                $request['duration'] = $started->diffInMilliseconds();
                 $request['code'] = $outboundRequestEvent instanceof ResponseReceived ? $outboundRequestEvent->response->status() : 0;
                 $request['status'] = 'completed';
                 $request['request'] = [
